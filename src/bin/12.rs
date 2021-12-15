28370559fc724e80a3d0060adef0e2828a262db7
Sean Flinn <sflinn54@gmail.com>
2021-12-14 22:27:04 -0800

    Completed day 12.

--- src/bin/12.rs
+++ src/bin/12.rs
@@ -1,11 +1,13 @@
 use std::cell::{RefCell, RefMut};
 use std::rc::Rc;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use anyhow::anyhow;
 
 use advent_of_code::parse_data_file;
 
 const DAYS: u16 = 256;
+const START: &'static str = "start";
+const END: &'static str = "end";
 
 fn parse_graph<'a>(input_string: &'a str) -> anyhow::Result<Rc<RefCell<Node<'a>>>> {
     let mut output = HashMap::new();
@@ -29,7 +31,43 @@ fn main() -> anyhow::Result<()> {
     let input_string = parse_data_file("12.txt")?;
     let graph = parse_graph(&input_string)?;
 
+    let mut visited = HashSet::new();
+    let answer = search(graph, &mut visited, true);
+
+    println!("The answer is {}", answer);
+
     Ok(())
+}
+
+fn search<'a>(node: Rc<RefCell<Node<'a>>>, visited: &mut HashSet<&'a str>, can_revisit: bool) -> u32 {
+    let node_val = node.borrow().val;
+
+    // end, return
+    if node_val == END {
+        return 1;
+    }
+
+    // lowercase
+    let is_lowercase = node_val.find(char::is_uppercase).is_none();
+    let mut revisit = false;
+    match (is_lowercase, visited.contains(node_val), node_val == START, can_revisit) {
+        (false, _, _, _) => (), // Ignore non lowercase str
+        (true, false, _, _) => { visited.insert(node_val); } // first time on node
+        (true, true, true, _) | (true, true, false, false) => return 0, // can't revisit START or already used revisit
+        (true, true, false, true) => revisit = true, // using revisit
+    };
+
+    let mut output = 0;
+    for edge in node.borrow().edges.iter() {
+        output += search(edge.clone(), visited, can_revisit && !revisit);
+    }
+
+    // remove this node from visited at the end if this isn't a revisit
+    if is_lowercase && !revisit {
+        visited.remove(node_val);
+    }
+
+    output
 }
 
 #[derive(Debug)]
